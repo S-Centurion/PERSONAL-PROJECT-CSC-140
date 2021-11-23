e79878a8baada46241f173eb1869df6e03cbfd75
S-Centurion <94206285+S-Centurion@users.noreply.github.com>
2021-11-22 20:37:56 -0800

    Add files via upload

--- Veithj Personal Project.cpp
+++ Veithj Personal Project.cpp
@@ -1,7 +1,7 @@
 // Veithj Personal Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //BLADE RUNNER: DIMINISHING RETURNS
 
-#include <iostream>
+//#include <iostream>
 /*#include "ConsoleColor.h"
 #include <windows.h>
 #include <mmsystem.h>
@@ -11,29 +11,32 @@
 #include "Text.h"
 */
 #include "Start.h"
+
+//#include "Test.h"
 /*
-#include "Test.h"
 #include "ActOne.h"
 */
+//#include "Start.h"
 
 using namespace std;
 
 
 int main()
 {
-    const int BREAK_L = 6000, BREAK_S = 2500, BREAK_VS = 1250;
-
     PlaySound(TEXT("End Titles.wav"), NULL, SND_ASYNC);
-    title();
-    system("CLS");
+    Title();
 
     help();
-    system("CLS");
 
     PlaySound(TEXT("Blade Runner Blues.wav"), NULL, SND_ASYNC);
-    intro();
+    Intro();
 
-    
+    PlaySound(TEXT("Blade Runner Blues.wav"), NULL, SND_ASYNC);
+    OpeningNarration();
+    ConversationWithLieu();
+
+    //test();
+    //testScene1();
 }
 
 
